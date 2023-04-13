106058803d5f26e3844fbc63089aecce2ba82535
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-04-13 23:47:55 +0800

    Update restart.sh

--- ssh/restart.sh
+++ ssh/restart.sh
@@ -24,8 +24,6 @@ systemctl restart ssrmu
 systemctl restart ws-tls
 systemctl restart ws-nontls
 systemctl restart xray.service
-systemctl restart vless-grpc
-systemctl restart vmess-grpc
 systemctl restart shadowsocks-libev
 systemctl restart xl2tpd
 systemctl restart pptpd
