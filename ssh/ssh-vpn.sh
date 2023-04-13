00b37744665d833fcfb212ff94a0d86590c2bbe7
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-04-14 05:35:12 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -446,7 +446,7 @@ wget -O slowdnsmenu "https://raw.githubusercontent.com/NevermoreSSH/hop/main/upd
 wget -O running "https://raw.githubusercontent.com/NevermoreSSH/hop/main/update/running.sh"
 wget -O updatemenu "https://raw.githubusercontent.com/NevermoreSSH/hop/main/update/updatemenu.sh"
 wget -O sl-fix "https://raw.githubusercontent.com/NevermoreSSH/hop/main/sslh-fix/sl-fix"
-wget -O sl-fix "https://raw.githubusercontent.com/NevermoreSSH/hop/main/SLDNS/install-sldns"
+wget -O install-sldns "https://raw.githubusercontent.com/NevermoreSSH/hop/main/SLDNS/install-sldns"
 
 chmod +x sl-fix
 chmod +x install-sldns
