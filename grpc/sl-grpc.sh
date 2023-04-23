6ee40f680932bf99bda8a5221fcfc925f31875f1
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-04-24 03:48:10 +0800

    Update sl-grpc.sh

--- grpc/sl-grpc.sh
+++ grpc/sl-grpc.sh
@@ -13,7 +13,7 @@ apt -y install chrony
 timedatectl set-ntp true
 systemctl enable chronyd && systemctl restart chronyd
 systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Jakarta
+timedatectl set-timezone Asia/Kuala_Lumpur
 chronyc sourcestats -v
 chronyc tracking -v
 date
