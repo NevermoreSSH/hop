add0053689f9b0a0fcbe98e930a0b31e5d44c2e3
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-11-29 16:15:48 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -538,6 +538,8 @@ chmod +x uservless
 echo "0 5 * * * root clearlog && reboot" >> /etc/crontab
 echo "0 1 * * * root delexp && xp && restart" >> /etc/crontab
 echo "0 4 * * * root clearlog && sslh-fix-reboot" >> /etc/crontab
+echo "0 6 * * * root backup" >> /etc/crontab
+echo "0 23 * * * root backup" >> /etc/crontab
 
 
 # remove unnecessary files
