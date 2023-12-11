3e1404cdbe0e0ed18a9a4ae750cf9f7483c3530b
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-11 13:43:56 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -12,6 +12,9 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
 # ==================================================
 # Link Hosting Kalian
 akbarvpn="raw.githubusercontent.com/NevermoreSSH/hop/main/ssh"
